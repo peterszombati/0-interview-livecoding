a52c8b96abe60a3a9518fe8640419c1d77f8335c
Peter Szombati <szombatipeti7@gmail.com>
2023-10-26 15:18:09 +0200

    solution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,39 @@
 import type { NextPage } from "next";
 import { useState, useEffect } from "react";
 
+// requirements: make it work :) doesnt matter how
+type Product = { name: string, price: number }
+type JsonResponse = Record<number, Product>
+type State = (Product & { key: string })[]
+
+function getApiHello() {
+  return fetch("/api/hello")
+    .then((response) => response.json())
+    .then((json: JsonResponse) => Object.entries(json).map(i => ({
+      key: i[0],
+      ...i[1],
+    })))
+}
+
 const Home: NextPage = () => {
-  const [products, setProducts] = useState<any[]>([]);
+  const [products, setProducts] = useState<State | null>(null);
 
   useEffect(() => {
-    fetch("/api/hello").then((response) => {});
+    getApiHello()
+      .then(result => setProducts(result))
+      .catch(e => console.error(e))
   }, []);
 
   return (
     <div>
-      {products.map((product) => {
+      {products ? products.map((product) => {
         return (
-          <div key={0}>
+          <div key={product.key}>
             <p>{product.name}</p>
             <p>{product.price}</p>
           </div>
         );
-      })}
+      }) : 'Loading'}
     </div>
   );
 };
